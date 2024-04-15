b183dd3af68d5da52a8b822f7631a9926cf8cfd8
Deyverson1 <deyversongp@gmail.com>
2024-04-15 10:58:54 -0500

    Imagen de fondo actualizada

--- src/dashboard/Carousel.tsx
+++ src/dashboard/Carousel.tsx
@@ -1,35 +1,63 @@
+import { useState } from 'react';
 import 'react-multi-carousel/lib/styles.css';
 import Carousel from 'react-multi-carousel';
 import GetStartedInput from '../components/GetStarted';
 
-
 export default function CarouselDashboard() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [valueIndex, setValueIndex] = useState(0)
+  const handleSlideChange = (index: number) => {
+    if(index - 2 === -1){
+      setCurrentSlide(2)
+    }else if(index - 2 === -2){
+      setCurrentSlide(1)
+    } else if (index -2 === 0){
+      setCurrentSlide(0)
+    } else{
+      setCurrentSlide(Math.abs(index - 2))
+    }
+    // console.log((index - 2))
+  };
+
   const responsive = {
     default: {
       breakpoint: { max: 4000, min: 1 },
       items: 1
     }
-  }
+  };
+
+  const backgrounds = [
+    'https://assets.nflxext.com/ffe/siteui/vlv3/c1366fb4-3292-4428-9639-b73f25539794/a7ec3522-a8e1-47be-a8e1-e12070dd3d5b/CO-en-20240408-popsignuptwoweeks-perspective_alpha_website_medium.jpg',
+    'https://occ-0-3933-3934.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABQ_e4eijzlYYEcteFvJlfOu-1HXsGM6HoXr2ZrlGL0_hDFLhfTCgYwrN8m557iDaMIFHY-Ym_ualbJBciFw8UctwXrq8IIU3Mvlp.webp?r=1e5',
+    'https://occ-0-3933-3934.1.nflxso.net/dnm/api/v6/Z-WHgqd_TeJxSuha8aZ5WpyLcX8/AAAABTQFHTx6huB9WmwuzkyAWnXRMp_kgLcgqSuVW7sYwEM6I7H14dTW3I_Mi2GyNSMFjBV4Dp2ltjvwO7QVtFwVH3J1YD3PjZ9DSzxj.webp?r=da9',
+    'https://assets.nflxext.com/ffe/siteui/vlv3/c1366fb4-3292-4428-9639-b73f25539794/a7ec3522-a8e1-47be-a8e1-e12070dd3d5b/CO-en-20240408-popsignuptwoweeks-perspective_alpha_website_medium.jpg',
+  ];
+
   return (
     <div className='w-full h-screen'>
-      <div className='flex flex-col items-center justify-center w-full h-full'  style={{ backgroundImage: 'url(https://assets.nflxext.com/ffe/siteui/vlv3/c1366fb4-3292-4428-9639-b73f25539794/a7ec3522-a8e1-47be-a8e1-e12070dd3d5b/CO-en-20240408-popsignuptwoweeks-perspective_alpha_website_medium.jpg)' }}>
+      <div
+        className='flex flex-col items-center justify-center w-full h-full bg-center bg-cover '
+        style={{ backgroundImage: `url(${backgrounds[currentSlide]})`, aspectRatio: '16/9'}}
+      >
         <Carousel
-          className='flex justify-start w-full px-40 '
+          className='flex justify-start w-full px-40'
           arrows={true}
           responsive={responsive}
           draggable={true}
           swipeable={true}
           autoPlay={false}
           autoPlaySpeed={3000}
-          // rewind={true}
           infinite={true}
+          beforeChange={handleSlideChange}
         >
-          <div className='w-5/12 pt-52'>
+
+          <div id="slide1" className='w-5/12 pt-52'>
             <h1 className='w-full text-6xl font-bold'>Unlimited movies, TV shows, and more</h1>
             <h2 className='pt-4 text-lg font-semibold'>Starts at COP 16,900. Cancel anytime.</h2>
             <GetStartedInput />
           </div>
-          <div className='flex flex-col w-5/12 pt-36 gap-y-3'>
+
+          <div id="slide2" className='flex flex-col w-5/12 pt-36 gap-y-3'>
             <button className='px-3 py-2 text-base font-semibold bg-black rounded-full w-fit bg-opacity-45'>🔥 Trending now</button>
             <img src="https://occ-0-3933-3934.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABWMKOE-FSQ7OtcO7lOVOdMJrLmmadxS4J_gCD24uv9rs3XulHZtKh5TEmKE0aRPgwcDcSqJriAfhdIj6x3gtMUcBGwGq3meu1QQ.webp?r=de5" className='w-6/12' alt="" />
             <section>
@@ -39,7 +67,8 @@ export default function CarouselDashboard() {
             </h2>
             <GetStartedInput />
           </div>
-          <div className='flex flex-col w-5/12 pt-44 gap-y-3'>
+
+          <div id="slide3" className='flex flex-col w-5/12 pt-44 gap-y-3'>
             <button className='px-3 py-2 text-base font-semibold bg-black rounded-full w-fit bg-opacity-45'>🔥 Trending now</button>
             <img src="https://occ-0-3933-3934.1.nflxso.net/dnm/api/v6/S4oi7EPZbv2UEPaukW54OORa0S8/AAAABbTtoL0ZQcYxosgpe139n9LQDdKnECWFE7AWYbfZBveNAyEPpcvzb_wqqm50prqpah1FT3yV5lBZ_6Z0MckcwDtLBS5RFVeLDQ.webp?r=a51" className='w-6/12' alt="" />
             <section>
@@ -52,5 +81,5 @@ export default function CarouselDashboard() {
         </Carousel>
       </div>
     </div>
-  )
+  );
 }
