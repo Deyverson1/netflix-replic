1326da80756f39c64ba716662950c37f2ef2f149
Deyverson1 <deyversongp@gmail.com>
2024-06-11 12:17:44 -0500

    Updated

--- src/Netflix/HeroStar.tsx
+++ src/Netflix/HeroStar.tsx
@@ -26,19 +26,19 @@ export default function HeroStar() {
       <div className="relative flex flex-col w-5/12 gap-y-4">
         <img src={randomItem.brand} className="w-8/12" alt="" />
         {
-          randomItem !== null && randomItem !== undefined && (
+          randomItem !== null && randomItem !== undefined && randomItem.top !== '' && (
             <div className="flex items-center gap-x-4">
               <div className="w-8">
                 <Top />
               </div>
-              <h1 className="text-xl font-bold">{randomItem.top}</h1>
+              <h1 className="text-xl font-bold text-white">{randomItem.top}</h1>
             </div>
           )
         }
-        <h2 className="w-11/12 text-lg">{randomItem.description}</h2>
+        <h2 className="w-11/12 tracking-tight text-md">{randomItem.description}</h2>
         <div className="flex gap-x-4">
-          <button className="flex items-center justify-center px-6 py-3 text-base font-bold text-black bg-white rounded-sm gap-x-2"><PlayerIcon /> Play</button>
-          <button className="flex items-center justify-center px-6 py-3 text-base font-bold text-white bg-gray-500 rounded-sm gap-x-2"><AlertIcon /> More info</button>
+          <button className="flex items-center justify-center px-6 py-2 text-base font-bold text-black bg-white rounded-md gap-x-2 hover:bg-gray-300"><PlayerIcon /> Play</button>
+          <button className="flex items-center justify-center px-6 py-2 text-base font-bold text-white rounded-md bg-opacity-80 bg-neutral-600 gap-x-2 hover:bg-opacity-60 hover:bg-neutral-700"><AlertIcon /> More info</button>
         </div>
       </div>
     </div>
